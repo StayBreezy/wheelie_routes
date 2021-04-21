228191ed338c22f7dfd3fd40d65a67fcc0a442ac
StayBreezy <justingross95@gmail.com>
2021-04-21 17:02:12 -0600

    keeping user logged in

--- src/App.js
+++ src/App.js
@@ -8,21 +8,18 @@ import Header from "./components/Header";
 import routes from "./routes";
 import { useDispatch, useSelector } from "react-redux";
 
-import {updateUser} from './redux/userReducer';
+import { updateUser } from "./redux/userReducer";
 import Upload from "./components/Upload";
 // import Filter from "./components/Filter";
 import Footer from "./components/Footer";
 import RoutePage from "./components/RoutePage";
 
-
 function App() {
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    axios.get('/api/auth/me')
-    .then(res =>
-      dispatch(updateUser(res.data)))
-  }, [])
+  useEffect(() => {
+    axios.get("/api/auth/me").then((res) => dispatch(updateUser(res.data)));
+  }, []);
   return <div className="mainApp">{routes}</div>;
 }
 
