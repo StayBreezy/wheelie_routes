6f4c688638b0c0bcbca61a9979096f08a82d9f7c
StayBreezy <justingross95@gmail.com>
2021-04-22 09:12:47 -0600

    added hamburger functionality with login/logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,9 @@ export default function Header(props) {
 
   const menu = () => {
     if(isOpen){
-      return <nav className="nav navSmall">
+      if(userState.isLoggedIn){
+
+        return <nav className="nav navSmall">
         <button className="noBtnBtn" onClick={() => setOpen(false)}>X</button>
       <div className="navLinks">
 
@@ -44,6 +46,28 @@ export default function Header(props) {
       </Link>
         </div>
     </nav>
+        } else {
+          return <nav className="nav navSmall">
+          <Link to="/" className="noLinkLink">
+            <p>Search</p>
+          </Link>
+          <Link to="/upload">
+            <p className="pLink" onClick={() => handleClick()}>
+              Upload
+            </p>
+          </Link>
+          {/* if no user then link to auth. if user link to profile and profile button */}
+          <p className="pLink" onClick={() => handleLogout()}>
+            logout
+          </p>
+          <Link
+            to={`/profile/${userState.username}`}
+            className="noLinkLink profileImg"
+          >
+            <p>Profile</p>
+          </Link>
+        </nav>
+        }
     } else {
       return <button className="menuBtn" onClick={() => setOpen(true)}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -65,7 +89,8 @@ export default function Header(props) {
         <Link to="/" className="noLinkLink">
           <h1 className="mainHeader">WheelieRoutes</h1>
         </Link>
-        <nav className="nav">
+        {menu()}
+        <nav className="nav navBig">
           <Link to="/" className="noLinkLink">
             <p>Search</p>
           </Link>
